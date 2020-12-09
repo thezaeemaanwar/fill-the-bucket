e51f3b2e04de88c6b1d1712077d5a92f4ac12a70
Zaeema Anwar <thezaeemaanwar@gmail.com>
2020-12-09 23:43:39 +0500

    Initial commit

---
+++ project1/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace project1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(14, 11);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "COLOUR";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(336, 11);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 43);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "CLOSE";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.trackBar1.Location = new System.Drawing.Point(132, 12);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(166, 45);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::project1.Properties.Resources.faucet;
+            this.pictureBox1.Location = new System.Drawing.Point(14, 88);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(92, 70);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(438, 505);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Fill Bucket";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ project1/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace project1
+{
+    public partial class Form1 : Form
+    {
+        private int tracbarValue;
+        private int height = 0;
+        private Timer timer = new Timer();
+        private Color color = Color.Cyan;
+        public Form1()
+        {
+            InitializeComponent();
+            this.Paint += new PaintEventHandler(frmMain_Paint);
+            timer.Interval = 100;
+            this.timer.Tick += timer_tick;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            color = colorDialog1.Color;
+        }
+        private void frmMain_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen p = new Pen(Color.White, 1);
+            SolidBrush b = new SolidBrush(color);
+            
+            // temporary rectangle for boundaries
+            Rectangle rectBoundary = new Rectangle(70, 200, 200, 250);
+
+            // Bucket for fluid to drop in
+            g.DrawLine(p, new Point(rectBoundary.X-1, rectBoundary.Y), new Point(rectBoundary.X-1, rectBoundary.Y+250));
+            g.DrawLine(p, new Point(rectBoundary.X+200, rectBoundary.Y), new Point(rectBoundary.X+200, rectBoundary.Y+250));
+            g.DrawLine(p, new Point(rectBoundary.X, rectBoundary.Y + 250), new Point(rectBoundary.X + 200, rectBoundary.Y + 250));
+            
+            // Rectanlge to fill the bucket
+            Rectangle fillingRect = new Rectangle(rectBoundary.X, (rectBoundary.Bottom-height), 200, height);
+            g.FillRectangle(b, fillingRect);
+            
+            // Handle the width of fluid dropping
+            if (tracbarValue > 0 && height < 240)
+            {
+                if (tracbarValue >= 7)
+                    g.FillRectangle(b, new Rectangle(82, 100, 20, 350));
+                else if (tracbarValue >= 3)
+                    g.FillRectangle(b, new Rectangle(85, 100, 15, 350));
+                else
+                    g.FillRectangle(b, new Rectangle(87, 100, 10, 350));
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // close application when close button is pressed
+            Application.Exit();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            // start the timer when trachbar value is greater than 0
+            if (tracbarValue > 0)
+            {
+                timer.Start();
+            }
+            // If trackbar value is 0, stop the timer
+            if (tracbarValue == 0)
+                timer.Stop();
+
+            // store trackBar value
+            tracbarValue = trackBar1.Value;
+        }
+        private void timer_tick(object sender, EventArgs e)
+        {
+            // On every timer tick, increase the height of rectangle
+            height += tracbarValue;
+            this.Invalidate();
+
+            // when rectangle is filled completely, 
+            // set trackbar value to 0 and stop the timer.
+            if (height >= 240)
+            {
+                trackBar1.Value = 0;
+                tracbarValue = 0;
+                height = 0;
+                timer.Stop();
+            }
+        }
+    }
+
+}
